c98f90a3278608c55e39e00f90801ed5ecaaf282
Flight <vladimirborisiuk@gmail.com>
2023-10-30 19:58:04 +0300

    feat(catalog): Brands

---
+++ app/Http/Resources/Catalog/BrandResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\Catalog;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BrandResource extends JsonResource
+{
+    public static $wrap = null;
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->whenHas('id'),
+            'name' => $this->whenHas('name'),
+            'slug' => $this->whenHas('slug'),
+        ];
+    }
+}
